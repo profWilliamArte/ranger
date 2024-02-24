197f736a81e12f812cd5bfd349b88a55ecdeeca8
profWilliamArte <arteagawilliam13@hotmail.com>
2024-02-24 15:06:22 -0400

    segundo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://profwilliamarte.github.io',/* aqui colocarias el nombre de tu cuenta*/
-    base: '/ranger',/* aqui colocarias el nombre de tu repositorio*/
+    //site: 'https://profwilliamarte.github.io',/* aqui colocarias el nombre de tu cuenta*/
+    //base: '/ranger',/* aqui colocarias el nombre de tu repositorio*/
     build: {
        assets: 'astro' /// muy importante
     }
